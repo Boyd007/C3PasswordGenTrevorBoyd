f34f4e4121cf687767d5529208d9c28bffae92f5
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-06-18 18:17:03 -0400

    updatedjs

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function generatePassword() {
   var uppercaseRequest = confirm("Press OK if you require upper case letters")
   var numericRequest = confirm("Press Ok if you wish to add numbers")
   var specialcharRequest = confirm("Press OK if you require any special characters")
-  var alphabet = "abcdefghijklmnopqrstuvwxyz"
-  var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var numbers = "0123456789"
-  var specialcharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  var passwordPool = ""
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  var alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',Z']
+  var numbers = ['0','1','2','3','4','5','6','7','8','9']
+  var specialcharacters = ['!','#','$','%','&','*','+',',','-','.','/',':',';','<=>','?','@','[\]','^','_',]
+  var passwordPool = [];
 if (lowercaseRequest) {
   passwordPool+=alphabet
 }
