a545dc047a82f2e481ecc7b9677a73416880341e
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-06-18 19:03:17 -0400

    updatedjs

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function generatePassword() {
   var numericRequest = confirm("Press Ok if you wish to add numbers")
   var specialcharRequest = confirm("Press OK if you require any special characters")
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  var alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',Z']
+  var alphabetUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   var numbers = ['0','1','2','3','4','5','6','7','8','9']
   var specialcharacters = ['!','#','$','%','&','*','+',',','-','.','/',':',';','<=>','?','@','[\]','^','_',]
   var passwordPool = [];
@@ -24,9 +24,8 @@ if (specialcharRequest) {
   passwordPool=passwordPool.concat(specialcharRequest)
 }
 for (var i = 0; i <= numOfChar; i++) {
-  var randomChar = Math.floor(Math.random() * passwordString.length);
-  newPassword += passwordString.charAt(randomChar);
-  console.log(newPassword)
+  passwordArray.push(characterArray[Math.floor(Math.random() * characterArray.length)]);
+  newPassword = passwordString.push(passwordPool[randomChar]) console.log(newPassword)
 }
 
 }
